0c538e99d149c8d7798768886fb360ed87a425f3
Christopher <joundanger@outlook.com>
2021-08-25 22:43:23 +0800

    Search Pattern

--- main.rs
+++ main.rs
@@ -1,27 +1,43 @@
+use std::io::BufRead;
 use std::io::BufReader;
 use std::fs::File;
 use std::io::Result;
 use std::env;
+use std::str;
 
 
 fn main() -> Result<()> {
 
 	let args : Vec<String> = env::args().collect();
-	let (filename, query) = args_analysis(args);
+	let (filename, query) = args_analysis(&args);
 
 	let file = File::open(filename)?;
 	let	mut buf = BufReader::new(file);
 
-	
+	search_buf(buf, "mut");
 
 	Ok(())
 
 }
 
-fn args_analysis(args : Vec<String>) -> (String, String) {
+fn args_analysis(args : &[String]) -> (String, String) {
+	let filename = &args[1];
+	let query = &args[2];
 
-	let filename = args[1];
-	let query = args[2];
+	(filename.clone(), query.clone())
+}
 
-	(filename, query)
+fn search_buf(buf : BufReader<File>, pattern : &str) {
+	for line in buf.lines() {
+		match line {
+			Ok(stri) => {
+				if stri.contains(pattern) {
+					println!("{}", stri);
+				}
+			}
+			Err(error) => {
+				return;
+			}
+		}
+	}
 }
