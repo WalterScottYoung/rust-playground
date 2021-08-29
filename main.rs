e5f1249acf1d99f96f933e97adfe129139a017dc
Christopher <joundanger@outlook.com>
2021-08-29 21:58:53 +0800

    Simple Server

--- main.rs
+++ main.rs
@@ -1,43 +1,101 @@
-use std::io::BufRead;
-use std::io::BufReader;
-use std::fs::File;
-use std::io::Result;
-use std::env;
-use std::str;
+use log::{error, info};
+use std::io::prelude::{Read, Write};
+use std::path::Path;
+use std::io::Error;
+use std::net::{TcpListener, TcpStream};
+use std::fs;
 
-
-fn main() -> Result<()> {
-
-	let args : Vec<String> = env::args().collect();
-	let (filename, query) = args_analysis(&args);
-
-	let file = File::open(filename)?;
-	let	mut buf = BufReader::new(file);
-
-	search_buf(buf, "mut");
-
-	Ok(())
-
+struct Request<'a> {
+	method 	: &'a str,
+	uri		: &'a Path,
+	http_version : &'a str,
 }
 
-fn args_analysis(args : &[String]) -> (String, String) {
-	let filename = &args[1];
-	let query = &args[2];
-
-	(filename.clone(), query.clone())
+impl <'a> std::fmt::Display for Request<'a> {
+	fn fmt(&self, f : &mut  std::fmt::Formatter<'_>) -> std::fmt::Result {
+		write!(
+			f,
+			"{} {} {}\r \n",
+			self.method,
+			self.uri.display(),
+			self.http_version
+		)
+	}
 }
 
-fn search_buf(buf : BufReader<File>, pattern : &str) {
-	for line in buf.lines() {
-		match line {
-			Ok(stri) => {
-				if stri.contains(pattern) {
-					println!("{}", stri);
-				}
-			}
-			Err(error) => {
-				return;
-			}
+fn main()  {
+	simple_logger::init().unwrap();
+	info!("Starting Server ... ");
+
+	let ip =  "192.168.137.3:8954";
+	let listener = TcpListener::bind(ip).expect("Unable to Create Listerner");
+	info!("Server started on : {} {}", "http://", ip);
+
+	for stream in listener.incoming() {
+		match stream {
+			Ok(stream) => match handle_connection(stream) {
+				Ok(_) => (),
+				Err(e) => error!("Error handling connection: {}", e),
+
+			},
+			Err(e) => error!("Connection failed: {}", e),
 		}
 	}
+}
+
+fn handle_connection(mut stream : TcpStream ) -> Result<(), Error> {
+	// buffer 
+	let mut buffer = [0; 512];
+
+	stream.read(&mut buffer).unwrap();
+
+	let request = String::from_utf8_lossy(&buffer[..]);
+	let request_line = request.lines().next().unwrap();
+
+	match parse_request_line(&request_line) {
+		Ok(request) => {
+			info!("Request {}", &request);
+
+			let contents = fs::read_to_string("/home/christopher/sources/rust/rust-playground/resources/index.html").unwrap();
+			let response = format!("{}{}", "HTTP/1.1 200 OK\r\n\r\n", contents);
+
+			info!("Response : {}", &response);
+			stream.write(response.as_bytes()).unwrap();
+			stream.flush().unwrap();	
+		}
+		Err(_) => error!("Badly formatted request: {}", &request_line),
+	}
+
+	Ok(())
+}
+
+fn parse_request_line(request : & str) -> Result<Request, Box<dyn std::error::Error>> {
+	let mut parts = request.split_whitespace();
+
+	let method = parts.next().ok_or("Method not specified")?;
+	// we only accept get requests
+	if method != "GET" {
+		Err("Unsupported method")?;
+	}
+
+	let uri = Path::new(parts.next().ok_or("Uri not specified")?);
+	let nrom_uri =  uri.to_str().expect("Invalid unicode !");
+
+	const ROOT: &str = "/home/christopher/sources/rust/rust-playground/resources";
+
+	if !Path::new(&format!("{}{}", ROOT, nrom_uri)).exists() {
+		Err("Requested resource does not exits")?;
+	}
+
+	let http_version = parts.next().ok_or("HTTP version not specified")?;
+	if http_version != "HTTP/1.1" {
+		Err("Unsupported HTTP version, use HTTP/1.1")?;
+	}
+
+	Ok(Request {
+		method,
+		uri,
+		http_version,
+	}
+	)
 }
---
+++ message.rs
@@ -0,0 +1,10 @@
+struct Item {
+	Name : String,
+	Value : String,
+}
+
+impl Item {
+	fn from_string(s : &[str]) -> Item {
+		
+	}
+}
